1db62ed13079669162e1f8fd487518c4d27ccb45
kaery1000 <km4350@nyu.edu>
2018-08-06 22:43:09 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express"),
+        app = express();
+        
+        
+app.get('/', (req, res) => {
+    res.send({hi :"there"});
+});
+
+app.listen(process.env.PORT, process.env.IP , () => {
+    console.log("The server has started");
+});
