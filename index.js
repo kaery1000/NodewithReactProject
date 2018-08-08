4352b73747c27b52174c025fdfddd7b9917c5e8e
kaery1000 <km4350@nyu.edu>
2018-08-08 19:04:12 +0000

    add landing and OAuth

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express"),
-        app = express();
+        app = express(),
+        // adding passport
+        passport = require("passport"),
+        // adding passport startegy
+        googleStrategy = require("passport-google-oauth20").Strategy,
+        // adding keys
+        keys = require("./config/keys");
         
-        
-app.get('/', (req, res) => {
-    res.send({hi :"there"});
-});
+// Passport config
+passport.use(
+    new googleStrategy(
+        {
+            clientID : keys.googleClientID,
+            clientSecret : keys.googleClientSecret,
+            callbackURL : "/auth/google/callback"
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log(accessToken);
+            console.log(refreshToken);
+            console.log(profile);
+        }
+    )
+);
+
+
+app.get(
+    "/auth/google",
+    passport.authenticate ("google", {
+        scope : ["profile", "email"]
+    })
+);
+app.get("/auth/google/callback", passport.authenticate("google"));
+
 
 app.listen(process.env.PORT, process.env.IP , () => {
     console.log("The server has started");
