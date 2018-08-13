1ca13e5f04439aa429eabc405cfb28a72e710622
kaery1000 <km4350@nyu.edu>
2018-08-13 18:44:54 +0000

    adding node-modules

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// Keys configs for dev environment
+module.exports = {
+    googleClientID : "132484085509-da3f60oak5ivfm31itemv61vko5r8v2h.apps.googleusercontent.com",
+    googleClientSecret : "s56MSPVs_uPOoJi3Zg2qufiU",
+    mongoURI : "mongodb://admin:admin18@06@ds215961.mlab.com:15961/emaily-dev-v1",
+    cookieKey : "youcanhavearandomstringaswell"
+};
